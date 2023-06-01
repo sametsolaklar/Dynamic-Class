3cdacb7a414859f6d0a99a67de103f4aac61a566
Samet <Samet@DESKTOP-KJFE73I>
2023-06-01 22:49:32 +0300

    Add project files.

---
+++ Dynamic-Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using DynamicClass;
+using static DynamicClass.DynamicClassProperty;
+
+var fields = new List<Temp>() {
+            new Temp("ID", typeof(int)),
+            new Temp("NAME", typeof(string)),
+            new Temp("SURNAME", typeof(string))
+        };
+dynamic obj = new DynamicClassProperty(fields);
+obj.ID = 123456;
+obj.NAME = "SAMET";
+obj.SURNAME = "SOLAKLAR";
+Console.WriteLine(obj.ID);     //123456
+Console.WriteLine(obj.NAME);   //SAMET
+Console.WriteLine(obj.SURNAME);    //SOLAKLAR
